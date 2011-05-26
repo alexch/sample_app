1a5c6cb73de6dc5cd7d5c4b80522474205aae3af
Alex Chaffee <alex@stinky.com>
2011-05-26 10:35:17 -0700

    initial checkin

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+gem 'rake', '0.8.7'
+gem 'rails', '3.0.7'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+gem 'nokogiri'
+group :development, :test do
+  gem 'webrat'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '142f8e470d28e61ef72add2c54dd28907931414f8ff484d596f76c90e4a8c5ad95859c18d14d9509b4a79e384dcfbfb98e05d090f97a635d4da4035202014ee7'
